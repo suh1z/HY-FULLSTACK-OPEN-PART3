2acb27626990ef7ba3e0a2fe0cb4e2728045797d
suh1z <jobo.holmberg@gmail.com>
2024-06-23 23:02:40 +0300

    part#

---
+++ frontend_puhelinluettelo/src/App.jsx
@@ -0,0 +1,205 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './Notifications.css';
+const baseUrl = 'http://localhost:3001/api/persons'
+
+const Filters = ({ filter, handleFilterChange }) => {
+  return (
+    <div>
+      <label>Search user</label>
+      <input value={filter} onChange={handleFilterChange} />
+    </div>
+  );
+};
+
+const Notification = ({ message, type }) => {
+  if (message === null) {
+    return null;
+  }
+
+  return (
+    <div className={`notification ${type}`}>
+    <div className="icon"></div>
+      {message}
+    </div>
+  );
+};
+
+const deletePerson = ({persons, person, setPersons, setDeleteMessage, setErrorMessage}) => {
+  if (!person) {
+    console.error("No person provided for deletion");
+  }
+  if (window.confirm(`Delete ${person.name}?`)) {
+    axios
+      .delete(`${baseUrl}/${person.id}`)
+      .then(() => {
+        setPersons(persons.filter(p => p.id !== person.id));
+        setDeleteMessage(`Deleted ${person.name} successfully from the Phonebook`);
+        setTimeout(() => {
+          setDeleteMessage(null);
+        }, 2000);
+      })
+  }
+};
+
+const Persons = ({ persons, filter, setPersons, deletePerson, setDeleteMessage}) => {
+  const filteredPersons = filter === ''
+    ? persons
+    : persons.filter(person =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      );
+
+  return (
+    <div>
+      <h3>Numbers</h3>
+      <ul>
+        {filteredPersons.map((person, index) => (
+          <li key={index}>{person.name} {person.number}
+          <button onClick={() => deletePerson({ person, persons, setPersons, setDeleteMessage })}>delete</button>         
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+
+const addNote = ({ newName, newNumber, persons, setPersons, setNewName, setNewNumber, setErrorMessage, setSuccessMessage, setChangeMessage }) => {
+  const existingPerson = persons.find(person => person.name && person.name.toLowerCase() === newName.toLowerCase());
+  if (existingPerson) {
+    if (window.confirm(`${newName} is already added to the phonebook! Do you want to update the number?`)) {
+      const updatedPerson = { id: existingPerson.id, name: existingPerson.name, number: newNumber };
+
+      axios
+        .put(`${baseUrl}/${existingPerson.id}`, updatedPerson)
+        .then(response => {
+          setPersons(persons.map(person => person.id !== existingPerson.id ? person : response.data));
+          setNewName('');
+          setNewNumber('');
+          setChangeMessage(`Changed ${newName}'s number successfully!`);
+          setTimeout(() => {
+            setChangeMessage(null);
+          }, 2000);
+        })
+        .catch(error => {
+          console.error("Update failed:", error);
+          setErrorMessage(`${newName} has been removed already.`);
+          setTimeout(() => {
+            setErrorMessage(null);
+          }, 2000);
+        });
+    }
+  } else {
+    const newPerson = { name: newName, number: newNumber };
+    axios
+      .post(baseUrl, newPerson)
+      .then(response => {
+        console.log('Response:', response.data);
+        setPersons([...persons, response.data]);
+        setNewName('');
+        setNewNumber('');
+        setSuccessMessage(`Added ${newPerson.name} successfully to the Phonebook`);
+        setTimeout(() => {
+          setSuccessMessage(null);
+        }, 2000);
+      })
+      .catch(error => {
+        console.error('Error adding new person:', error);
+        if (error.response) {
+          console.error('Server Error:', error.response.data);
+          setErrorMessage(error.response.data.error);
+        } else {
+          console.error('Network Error:', error.message);
+          setErrorMessage('Network Error: Unable to connect to the server');
+        }
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 2000);
+      });
+  }
+};
+
+const PersonForm = ({ newName, newNumber, handleNameChange, handleNumberChange, handleSubmit }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name:</label>
+        <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        <label>Number:</label>
+        <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <button type="submit">add</button>
+    </form>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([]); 
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+  const [notes, setNotes] = useState([])
+  const [changeMessage, setChangeMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [deleteMessage, setDeleteMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get(`${baseUrl}`)
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data);
+      })
+  }, [])
+
+  console.log('render', notes.length, 'notes')
+      
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+  
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addNote({ newName, newNumber, persons, setPersons, setNewName, 
+      setNewNumber, setSuccessMessage, setChangeMessage, setErrorMessage });
+  };
+  
+  return (
+    <div>
+      <Notification message={changeMessage} type="change" />
+      <Notification message={successMessage} type="success" />
+      <Notification message={deleteMessage} type="delete" />
+      <Notification message={errorMessage} type="error" />
+      <h2>Phonebook</h2>
+      <Filters filter={filter} handleFilterChange={handleFilterChange} />
+      <h2>Add new</h2>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        handleSubmit={handleSubmit}/>
+      <Persons 
+        persons={persons} 
+        filter={filter} 
+        deletePerson={deletePerson} 
+        setPersons={setPersons}
+        setDeleteMessage={setDeleteMessage}
+        setErrorMessage={setErrorMessage}  />
+      </div>
+  );
+};
+
+export default App;
---
+++ index.js
@@ -0,0 +1,117 @@
+const express = require('express')
+var morgan = require('morgan')
+const app = express()
+app.use(express.json());
+const cors = require('cors')
+app.use(cors())
+
+
+
+let persons = [
+    {
+      id: "1",
+      name: "HTML is easy",
+      number: "123"
+    }
+  ]
+
+  morgan.token('body', (req, res) => {
+    if (req.body) {
+        return JSON.stringify(req.body);
+    }
+    return;
+});
+
+const tinyJson = ':method :url :status :res[content-length] - :response-time ms :body';
+
+app.use(morgan(tinyJson));
+
+  const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => Number(n.id)))
+      : 0
+    return String(maxId + 1)
+  }
+  
+  app.get('/api/info', (request, response) => {
+    const personCount = persons.length;
+    const currentDate = new Date();
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  
+    response.send(`
+      <p>There are ${personCount} persons in phonebook.</p>
+      <p>${currentDate} ${timeZone}.</p>
+    `);
+  });
+
+  app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(note => note.id === id)
+    
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
+
+  app.put('/api/persons/:id', (req, res) => {
+    const updatedData = req.body;
+    const personIndex = persons.findIndex(person => person.id === updatedData.id);
+  
+    if (personIndex === -1) {
+      return res.status(404).json({ error: 'Person not found' });
+    }
+  
+    const updatedPerson = { ...persons[personIndex], ...updatedData };
+    persons[personIndex] = updatedPerson;
+  
+    res.status(200).json(updatedPerson);
+  });
+  
+
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'Number or Name missing' 
+      })
+    }
+
+    const personExists = persons.some(person => person.name === body.number);
+
+    if (personExists) {
+      return response.status(400).json({
+        error: 'Person already exists'
+      });
+    }
+
+    const NewPerson = {
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    }
+
+    persons = persons.concat(NewPerson)
+  
+    response.json(NewPerson)
+    
+  })
+
+  
+
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
